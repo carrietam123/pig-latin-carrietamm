a40d121443b3cb525109639d41349b8f9cb427f5
carrietamm <143368136+carrietamm@users.noreply.github.com>
2024-02-07 12:52:39 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -28,8 +28,8 @@ public class PigLatin {
 	    }
     }
     public int findFirstVowel(String sWord) {
-          for(int i = 0; i < word.length(); i++)
-    if(word.substring( i,i+1).equals ("e") || word.substring( i,i+1).equals ("a") || word.substring( i,i+1).equals ("i") ||word.substring( i,i+1).equals ("o") ||word.substring( i,i+1).equals ("u") ) 
+          for(int i = 0; i < sWord.length(); i++)
+    if(sWord.substring( i,i+1).equals ("e") || sWord.substring( i,i+1).equals ("a") || sWord.substring( i,i+1).equals ("i") ||sWord.substring( i,i+1).equals ("o") ||sWord.substring( i,i+1).equals ("u") ) 
     {
 return i;
       
@@ -47,10 +47,10 @@ return i;
 	    if(sWord.substring(0,2).equals("qu")){
 		    return(sWord.substring(2, sWord.length())+"quay");
 	    }
-	if(sWord.substring( 0,1).equals ("e") || sWord.substring( 0,1).equals ("a") || sWord.substring( 0,1).equals ("i") ||sWord.substring( 0,1).equals ("o") ||sWord.substring(0,1).equals ("u") ){
+	if(sWord.substring( 0,1).equals("e") || sWord.substring( 0,1).equals("a") || sWord.substring( 0,1).equals("i") ||sWord.substring( 0,1).equals("o") ||sWord.substring(0,1).equals("u") ){
 			return(sWord + "way");}
 		 for(int i = 0; i < word.length(); i++){
-   			 if(!sWord.substring( i,i+1).equals ("e") || !sWord.substring( i,i+1).equals ("a") || !sWord.substring( i,i+1).equals ("i") ||!sWord.substring( i,i+1).equals ("o") || !sWord.substring( i,i+1).equals ("u") ){
+   			 if(!sWord.substring( i,i+1).equals("e") || !sWord.substring( i,i+1).equals("a") || !sWord.substring( i,i+1).equals("i") ||!sWord.substring( i,i+1).equals("o") || !sWord.substring( i,i+1).equals("u") ){
     	return sWord + "ay";}
 		 }
 	    
